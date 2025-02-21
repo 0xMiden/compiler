8d29070201b14991e211d395a711bca4d79dcc6b
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2025-02-21 09:31:03 +0200

    feature(frontend): generate synthetic functions with Miden ABI transformation (stdlib, tx_kernel);

--- tests/integration/src/rust_masm_tests/abi_transform/stdlib.rs
+++ tests/integration/src/rust_masm_tests/abi_transform/stdlib.rs
@@ -16,7 +16,6 @@ use proptest::{
 
 use crate::CompilerTest;
 
-#[ignore = "until https://github.com/0xPolygonMiden/compiler/issues/352 is fixed"]
 #[test]
 fn test_blake3_hash() {
     let main_fn =
@@ -30,57 +29,59 @@ fn test_blake3_hash() {
     );
     // Test expected compilation artifacts
     test.expect_wasm(expect_file![format!("../../../expected/{artifact_name}.wat")]);
-    test.expect_ir(expect_file![format!("../../../expected/{artifact_name}.hir")]);
+    test.expect_ir2(expect_file![format!("../../../expected/{artifact_name}.hir")]);
     test.expect_masm(expect_file![format!("../../../expected/{artifact_name}.masm")]);
 
-    let package = test.compiled_package();
-
-    // Run the Rust and compiled MASM code against a bunch of random inputs and compare the results
-    let res = TestRunner::default().run(&any::<[u8; 32]>(), move |ibytes| {
-        let hash_bytes = blake3::hash(&ibytes);
-        let rs_out = hash_bytes.as_bytes();
-        let in_addr = 21u32 * 65536;
-        let out_addr = 20u32 * 65536;
-        let mut frame = Vec::<Felt>::default();
-        // Convert input bytes to words
-        let words = midenc_debug::bytes_to_words(&ibytes);
-        for word in words.into_iter().rev() {
-            PushToStack::try_push(&word, &mut frame);
-        }
-        PushToStack::try_push(&2u32, &mut frame); // num_words
-        PushToStack::try_push(&(in_addr / 16), &mut frame); // dest_ptr
-        dbg!(&ibytes, &frame, rs_out);
-        // Arguments are: [hash_output_ptr, hash_input_ptr]
-        let mut exec = Executor::for_package(
-            &package,
-            // Place the hash output at 20 * PAGE_SIZE, and the hash input at 21 * PAGE_SIZE
-            vec![Felt::new(in_addr as u64), Felt::new(out_addr as u64)],
-            &test.session,
-        )
-        .map_err(|err| TestCaseError::fail(err.to_string()))?;
-        // Reverse the stack contents, so that the correct order is preserved after
-        // MemAdviceProvider does its own reverse
-        frame.reverse();
-        let advice_inputs = AdviceInputs::default().with_stack(frame);
-        exec.with_advice_inputs(advice_inputs);
-        let trace = exec.execute(&package.unwrap_program(), &test.session);
-        let vm_in: [u8; 32] = trace
-            .read_from_rust_memory(in_addr)
-            .expect("expected memory to have been written");
-        dbg!(&vm_in);
-        let vm_out: [u8; 32] = trace
-            .read_from_rust_memory(out_addr)
-            .expect("expected memory to have been written");
-        dbg!(&vm_out);
-        prop_assert_eq!(rs_out, &vm_out, "VM output mismatch");
-        Ok(())
-    });
-
-    match res {
-        Err(TestError::Fail(_, value)) => {
-            panic!("Found minimal(shrinked) failing case: {:?}", value);
-        }
-        Ok(_) => (),
-        _ => panic!("Unexpected test result: {:?}", res),
-    }
+    // FIX: uncomment when https://github.com/0xPolygonMiden/compiler/issues/352 is fixed
+    //
+    // let package = test.compiled_package();
+    //
+    // // Run the Rust and compiled MASM code against a bunch of random inputs and compare the results
+    // let res = TestRunner::default().run(&any::<[u8; 32]>(), move |ibytes| {
+    //     let hash_bytes = blake3::hash(&ibytes);
+    //     let rs_out = hash_bytes.as_bytes();
+    //     let in_addr = 21u32 * 65536;
+    //     let out_addr = 20u32 * 65536;
+    //     let mut frame = Vec::<Felt>::default();
+    //     // Convert input bytes to words
+    //     let words = midenc_debug::bytes_to_words(&ibytes);
+    //     for word in words.into_iter().rev() {
+    //         PushToStack::try_push(&word, &mut frame);
+    //     }
+    //     PushToStack::try_push(&2u32, &mut frame); // num_words
+    //     PushToStack::try_push(&(in_addr / 16), &mut frame); // dest_ptr
+    //     dbg!(&ibytes, &frame, rs_out);
+    //     // Arguments are: [hash_output_ptr, hash_input_ptr]
+    //     let mut exec = Executor::for_package(
+    //         &package,
+    //         // Place the hash output at 20 * PAGE_SIZE, and the hash input at 21 * PAGE_SIZE
+    //         vec![Felt::new(in_addr as u64), Felt::new(out_addr as u64)],
+    //         &test.session,
+    //     )
+    //     .map_err(|err| TestCaseError::fail(err.to_string()))?;
+    //     // Reverse the stack contents, so that the correct order is preserved after
+    //     // MemAdviceProvider does its own reverse
+    //     frame.reverse();
+    //     let advice_inputs = AdviceInputs::default().with_stack(frame);
+    //     exec.with_advice_inputs(advice_inputs);
+    //     let trace = exec.execute(&package.unwrap_program(), &test.session);
+    //     let vm_in: [u8; 32] = trace
+    //         .read_from_rust_memory(in_addr)
+    //         .expect("expected memory to have been written");
+    //     dbg!(&vm_in);
+    //     let vm_out: [u8; 32] = trace
+    //         .read_from_rust_memory(out_addr)
+    //         .expect("expected memory to have been written");
+    //     dbg!(&vm_out);
+    //     prop_assert_eq!(rs_out, &vm_out, "VM output mismatch");
+    //     Ok(())
+    // });
+    //
+    // match res {
+    //     Err(TestError::Fail(_, value)) => {
+    //         panic!("Found minimal(shrinked) failing case: {:?}", value);
+    //     }
+    //     Ok(_) => (),
+    //     _ => panic!("Unexpected test result: {:?}", res),
+    // }
 }
--- tests/integration/src/rust_masm_tests/abi_transform/tx_kernel.rs
+++ tests/integration/src/rust_masm_tests/abi_transform/tx_kernel.rs
@@ -20,7 +20,6 @@ fn setup_log() {
 }
 
 #[test]
-#[ignore = "temporarily disabled until we figure out what broke"]
 fn test_get_inputs_4() -> Result<(), Report> {
     test_get_inputs("4", vec![u32::MAX.into(), Felt::ONE, Felt::ZERO, u32::MAX.into()])
 }
@@ -51,41 +50,55 @@ end
 
     // Test expected compilation artifacts
     test.expect_wasm(expect_file![format!("../../../expected/{artifact_name}.wat")]);
-    test.expect_ir(expect_file![format!("../../../expected/{artifact_name}.hir")]);
-    test.expect_masm(expect_file![format!("../../../expected/{artifact_name}.masm")]);
+    test.expect_ir2(expect_file![format!("../../../expected/{artifact_name}.hir")]);
 
-    let package = test.compiled_package();
+    // FIX: temporarily disabled until we figure out what broke
 
-    // Provide a place in memory where the vector returned by `get_inputs` should be stored
-    let out_addr = 18u32 * 65536;
-    let exec = Executor::for_package(&package, vec![Felt::new(out_addr as u64)], &test.session)?;
-    let trace = exec.execute(&package.unwrap_program(), &test.session);
-    // Verify that the vector contains the expected elements:
+    // test.expect_masm(expect_file![format!("../../../expected/{artifact_name}.masm")]);
+    // let package = test.compiled_package();
     //
-    // Rust lays out the vector struct as follows (lowest addressed bytes first):
+    // // Provide a place in memory where the vector returned by `get_inputs` should be stored
+    // let out_addr = 18u32 * 65536;
+    // let exec = Executor::for_package(&package, vec![Felt::new(out_addr as u64)], &test.session)?;
+    // let trace = exec.execute(&package.unwrap_program(), &test.session);
+    // // Verify that the vector contains the expected elements:
+    // //
+    // // Rust lays out the vector struct as follows (lowest addressed bytes first):
+    // //
+    // //     [capacity, buf_ptr, len]
+    // //
+    // // 1. Extract the data pointer and length from the vector written to out_addr
+    // let data_ptr = trace.read_memory_element(out_addr / 16, 1).unwrap().as_int() as u32;
+    // assert_ne!(data_ptr, 0, "expected non-null data pointer");
+    // dbg!(data_ptr);
+    // let len = trace.read_memory_element(out_addr / 16, 2).unwrap().as_int() as usize;
+    // assert_eq!(
+    //     len,
+    //     expected_inputs.len(),
+    //     "expected vector to contain all of the expected inputs"
+    // );
+    // // 2. Read the vector elements via data_ptr and ensure they match the inputs
+    // dbg!(len);
+    // let word = trace.read_memory_word(data_ptr / 16).unwrap();
+    // assert_eq!(
+    //     word.as_slice(),
+    //     expected_inputs.as_slice(),
+    //     "expected vector contents to match inputs"
+    // );
     //
-    //     [capacity, buf_ptr, len]
-    //
-    // 1. Extract the data pointer and length from the vector written to out_addr
-    let data_ptr = trace.read_memory_element(out_addr / 16, 1).unwrap().as_int() as u32;
-    assert_ne!(data_ptr, 0, "expected non-null data pointer");
-    dbg!(data_ptr);
-    let len = trace.read_memory_element(out_addr / 16, 2).unwrap().as_int() as usize;
-    assert_eq!(
-        len,
-        expected_inputs.len(),
-        "expected vector to contain all of the expected inputs"
-    );
-    // 2. Read the vector elements via data_ptr and ensure they match the inputs
-    dbg!(len);
-    let word = trace.read_memory_word(data_ptr / 16).unwrap();
-    assert_eq!(
-        word.as_slice(),
-        expected_inputs.as_slice(),
-        "expected vector contents to match inputs"
-    );
-
-    // let ir_program = test.ir_masm_program();
-    // let emul_out = execute_emulator(ir_program.clone(), &[]);
+    // // let ir_program = test.ir_masm_program();
+    // // let emul_out = execute_emulator(ir_program.clone(), &[]);
     Ok(())
+}
+
+#[test]
+fn test_get_id() {
+    let main_fn = "() -> Felt { miden::account::get_id().into() }";
+    let artifact_name = "abi_transform_tx_kernel_get_id";
+    let test_builder =
+        CompilerTestBuilder::rust_fn_body_with_sdk(artifact_name, main_fn, true, None);
+    let mut test = test_builder.build();
+    // Test expected compilation artifacts
+    test.expect_wasm(expect_file![format!("../../../expected/{artifact_name}.wat")]);
+    test.expect_ir2(expect_file![format!("../../../expected/{artifact_name}.hir")]);
 }
