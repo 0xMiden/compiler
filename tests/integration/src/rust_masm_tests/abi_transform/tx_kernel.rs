22f60b5ec9973d8d4ca030a01ef9316eda718c62
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2025-03-04 10:49:11 -0500

    test: ignore component-based tests temporarily

--- tests/integration/src/rust_masm_tests/abi_transform/tx_kernel.rs
+++ tests/integration/src/rust_masm_tests/abi_transform/tx_kernel.rs
@@ -20,6 +20,7 @@ fn setup_log() {
 }
 
 #[test]
+#[ignore = "need type informatin for masm procedures"]
 fn test_get_inputs_4() -> Result<(), Report> {
     test_get_inputs("4", vec![u32::MAX.into(), Felt::ONE, Felt::ZERO, u32::MAX.into()])
 }
