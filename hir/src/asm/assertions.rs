08bf9893db47663ca90c5ae69e49baeaceceea8c
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2025-03-19 09:04:37 -0400

    chore: remove old contents of hir, hir-analysis, hir-transform

--- hir/src/asm/assertions.rs
+++
@@ -1,4 +0,0 @@
-//! This module contains the set of compiler-emitted assertion codes, along with their explanations
-
-/// This assertion fails when a pointer address does not meet minimum alignment for the type
-pub const ASSERT_FAILED_ALIGNMENT: u32 = 0xfa;
