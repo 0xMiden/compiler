9060ce1f70973384d759cb172f1c1b8a02731486
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-06-25 16:21:17 +0300

    chore: clean up debugging asserts

--- frontend/wasm/src/component/lift_exports.rs
+++ frontend/wasm/src/component/lift_exports.rs
@@ -1,7 +1,6 @@
 use alloc::rc::Rc;
 use core::cell::RefCell;
 
-use midenc_dialect_arith::ArithOpBuilder;
 use midenc_dialect_cf::ControlFlowOpBuilder;
 use midenc_dialect_hir::HirOpBuilder;
 use midenc_hir::{
@@ -200,11 +199,6 @@ fn generate_lifting_with_transformation(
     let result_type = &export_func_ty.results[0];
 
     load(&mut fb, result_ptr, result_type, &mut return_values, span)?;
-
-    // TODO: remove
-    let _value = fb.imm(midenc_hir::Immediate::Felt(miden_core::Felt::new(1)), span);
-    // fb.assertz(value, span)?;
-    // fb.breakpoint(span)?;
 
     assert!(
         return_values.len() <= 16,
