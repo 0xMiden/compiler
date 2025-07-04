b1ba41f8a6678cee5a609e0c1d0c9910d5550e6f
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-07-04 12:55:04 +0300

    refactor: make `miden_stdlib_sys::Digest` a newtype instead of type alias

--- sdk/stdlib-sys/src/intrinsics/crypto.rs
+++ sdk/stdlib-sys/src/intrinsics/crypto.rs
@@ -2,7 +2,58 @@
 
 use crate::{Felt, Word};
 
-pub type Digest = crate::Word;
+/// A cryptographic digest representing a 256-bit hash value.
+///
+/// This is a wrapper around `Word` which contains 4 field elements.
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+#[repr(transparent)]
+pub struct Digest {
+    pub inner: Word,
+}
+
+impl Digest {
+    /// Creates a new `Digest` from a `[Felt; 4]` array.
+    #[inline]
+    pub fn new(felts: [Felt; 4]) -> Self {
+        Self {
+            inner: Word::from(felts),
+        }
+    }
+
+    /// Creates a new `Digest` from a `Word`.
+    #[inline]
+    pub const fn from_word(word: Word) -> Self {
+        Self { inner: word }
+    }
+}
+
+impl From<Word> for Digest {
+    #[inline]
+    fn from(word: Word) -> Self {
+        Self::from_word(word)
+    }
+}
+
+impl From<Digest> for Word {
+    #[inline]
+    fn from(digest: Digest) -> Self {
+        digest.inner
+    }
+}
+
+impl From<[Felt; 4]> for Digest {
+    #[inline]
+    fn from(felts: [Felt; 4]) -> Self {
+        Self::new(felts)
+    }
+}
+
+impl From<Digest> for [Felt; 4] {
+    #[inline]
+    fn from(digest: Digest) -> Self {
+        digest.inner.into()
+    }
+}
 
 #[link(wasm_import_module = "miden:core-intrinsics/intrinsics-crypto@1.0.0")]
 extern "C" {
@@ -36,7 +87,7 @@ extern "C" {
 ///
 /// This directly maps to the `hmerge` VM instruction.
 #[inline]
-pub fn merge(digests: [Word; 2]) -> Word {
+pub fn merge(digests: [Digest; 2]) -> Digest {
     unsafe {
         let mut ret_area = ::core::mem::MaybeUninit::<Word>::uninit();
         let ptr = ret_area.as_mut_ptr() as *mut Felt;
@@ -44,18 +95,18 @@ pub fn merge(digests: [Word; 2]) -> Word {
         // see
         // https://0xmiden.github.io/miden-docs/imported/miden-vm/src/user_docs/assembly/cryptographic_operations.html
         extern_hmerge(
-            digests[1][0].inner,
-            digests[1][1].inner,
-            digests[1][2].inner,
-            digests[1][3].inner,
-            digests[0][0].inner,
-            digests[0][1].inner,
-            digests[0][2].inner,
-            digests[0][3].inner,
+            digests[1].inner[0].inner,
+            digests[1].inner[1].inner,
+            digests[1].inner[2].inner,
+            digests[1].inner[3].inner,
+            digests[0].inner[0].inner,
+            digests[0].inner[1].inner,
+            digests[0].inner[2].inner,
+            digests[0].inner[3].inner,
             ptr,
         );
 
         // Word::from(result)
-        ret_area.assume_init()
+        Digest::from_word(ret_area.assume_init())
     }
 }
