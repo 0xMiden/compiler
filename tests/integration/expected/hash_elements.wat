b31169b62a23ddb2392e13563647011710d5269d
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-07-08 09:02:52 +0300

    test: make `test_hash_elements` code under test return the first felt

--- tests/integration/expected/hash_elements.wat
+++ tests/integration/expected/hash_elements.wat
@@ -1,50 +1,55 @@
 (module $hash_elements.wasm
   (type (;0;) (func (param i32 i32 i32)))
-  (type (;1;) (func (param i32 f32 f32 f32 f32 f32 f32 f32 f32)))
+  (type (;1;) (func (param f32 f32 f32 f32 f32 f32 f32 f32) (result f32)))
   (import "miden:core-import/stdlib-crypto-hashes-rpo@1.0.0" "hash-memory" (func $miden_stdlib_sys::stdlib::crypto::hashes::extern_hash_memory (;0;) (type 0)))
   (table (;0;) 1 1 funcref)
   (memory (;0;) 16)
   (global $__stack_pointer (;0;) (mut i32) i32.const 1048576)
   (export "memory" (memory 0))
   (export "entrypoint" (func $entrypoint))
-  (func $entrypoint (;1;) (type 1) (param i32 f32 f32 f32 f32 f32 f32 f32 f32)
-    (local i32)
+  (func $entrypoint (;1;) (type 1) (param f32 f32 f32 f32 f32 f32 f32 f32) (result f32)
+    (local i32 i32 i32)
     global.get $__stack_pointer
-    i32.const 32
-    i32.sub
-    local.tee 9
-    global.set $__stack_pointer
-    local.get 9
+    local.tee 8
+    local.set 9
     local.get 8
-    f32.store offset=28
-    local.get 9
-    local.get 7
-    f32.store offset=24
-    local.get 9
-    local.get 6
-    f32.store offset=20
-    local.get 9
-    local.get 5
-    f32.store offset=16
-    local.get 9
-    local.get 4
-    f32.store offset=12
-    local.get 9
-    local.get 3
-    f32.store offset=8
-    local.get 9
-    local.get 2
-    f32.store offset=4
-    local.get 9
-    local.get 1
-    f32.store
-    local.get 9
+    i32.const 64
+    i32.sub
+    i32.const -32
+    i32.and
+    local.tee 10
+    global.set $__stack_pointer
+    i32.const 0
+    local.set 8
+    block ;; label = @1
+      loop ;; label = @2
+        local.get 8
+        i32.const 32
+        i32.eq
+        br_if 1 (;@1;)
+        local.get 10
+        local.get 8
+        i32.add
+        local.get 0
+        f32.store
+        local.get 8
+        i32.const 4
+        i32.add
+        local.set 8
+        br 0 (;@2;)
+      end
+    end
+    local.get 10
     i32.const 8
-    local.get 0
-    call $miden_stdlib_sys::stdlib::crypto::hashes::extern_hash_memory
-    local.get 9
+    local.get 10
     i32.const 32
     i32.add
+    call $miden_stdlib_sys::stdlib::crypto::hashes::extern_hash_memory
+    local.get 10
+    f32.load offset=32
+    local.set 0
+    local.get 9
     global.set $__stack_pointer
+    local.get 0
   )
 )
--- tests/integration/src/rust_masm_tests/abi_transform/stdlib.rs
+++ tests/integration/src/rust_masm_tests/abi_transform/stdlib.rs
@@ -87,11 +87,13 @@ fn test_blake3_hash() {
 
 #[test]
 fn test_hash_elements() {
-    // TODO: when this test is green add another test for arbitrary input lenght
+    // TODO: when this test is green add another test for arbitrary input length
     let main_fn = r#"
-    (f0: miden_stdlib_sys::Felt, f1: miden_stdlib_sys::Felt, f2: miden_stdlib_sys::Felt, f3: miden_stdlib_sys::Felt, f4: miden_stdlib_sys::Felt, f5: miden_stdlib_sys::Felt, f6: miden_stdlib_sys::Felt, f7: miden_stdlib_sys::Felt) -> miden_stdlib_sys::Digest {
-        let input = [f0, f1, f2, f3, f4, f5, f6, f7];
-        miden_stdlib_sys::hash_elements(&input)
+    (f0: miden_stdlib_sys::Felt, f1: miden_stdlib_sys::Felt, f2: miden_stdlib_sys::Felt, f3: miden_stdlib_sys::Felt, f4: miden_stdlib_sys::Felt, f5: miden_stdlib_sys::Felt, f6: miden_stdlib_sys::Felt, f7: miden_stdlib_sys::Felt) -> miden_stdlib_sys::Felt {
+        // let input = [f0, f1, f2, f3, f4, f5, f6, f7];
+        let input = [f0, f0, f0, f0, f0, f0, f0, f0];
+        let res = miden_stdlib_sys::hash_elements(&input);
+        res.inner.inner.0
     }"#
     .to_string();
     let config = WasmTranslationConfig::default();
@@ -108,6 +110,7 @@ fn test_hash_elements() {
     let config = proptest::test_runner::Config::with_cases(1);
     let res = TestRunner::new(config).run(&any::<[midenc_debug::Felt; 8]>(), move |test_felts| {
         let raw_felts: Vec<Felt> = test_felts.into_iter().map(From::from).collect();
+        dbg!(&raw_felts);
         // Compute expected hash using miden-core's Rpo256::hash_elements
         let expected_digest = miden_core::crypto::hash::Rpo256::hash_elements(&raw_felts);
         let expected_felts: [TestFelt; 4] = [
@@ -118,8 +121,8 @@ fn test_hash_elements() {
         ];
         dbg!(&expected_felts);
 
-        // Place the hash output at 20 * PAGE_SIZE, and the hash input at 21 * PAGE_SIZE
-        let out_addr = 20u32 * 65536;
+        // Place the hash output at 20 * PAGE_SIZE
+        let out_addr = 30u32 * 65536;
 
         let args = [
             raw_felts[0],
@@ -130,15 +133,12 @@ fn test_hash_elements() {
             raw_felts[5],
             raw_felts[6],
             raw_felts[7],
-            Felt::new(out_addr as u64),
         ];
 
         eval_package::<Felt, _, _>(&package, [], &args, &test.session, |trace| {
-            let vm_out: [midenc_debug::Felt; 4] = trace
-                .read_from_rust_memory(out_addr)
-                .expect("expected memory to have been written");
-            dbg!(&vm_out);
-            prop_assert_eq!(&expected_felts, &vm_out, "VM output mismatch");
+            let res: Felt = trace.parse_result().unwrap();
+            dbg!(res);
+            prop_assert_eq!(res, expected_digest[0]);
             Ok(())
         })?;
 
