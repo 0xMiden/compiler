bf5f790521fb6f0a56cce8a070f829b63ecac2f6
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-07-30 12:08:44 +0300

    test: remove `Scenario` testing harness for testnet

--- tests/integration-node/src/lib.rs
+++ tests/integration-node/src/lib.rs
@@ -1,9 +1,6 @@
 //! Integration tests that are deploying code and runnning test scenarior on a local Miden node instance or testnet
 
 pub mod local_node;
-pub mod scenario;
 
 #[cfg(test)]
 mod node_tests;
-#[cfg(test)]
-mod scenario_testnet_tests;
