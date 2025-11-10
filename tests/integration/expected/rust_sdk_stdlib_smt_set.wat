234b0301c83e1c02e44726a8f606941cfc033cc1
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-11-10 14:01:30 +0200

    feature: add stdlib `std::collections::smt` bindings to the SDK

---
+++ tests/integration/expected/rust_sdk_stdlib_smt_set.wat
@@ -0,0 +1,242 @@
+(module $rust_sdk_stdlib_smt_set.wasm
+  (type (;0;) (func (param i32 i32) (result i32)))
+  (type (;1;) (func (param f32 f32 f32 f32 f32 f32 f32 f32 f32 f32 f32 f32)))
+  (type (;2;) (func (param i32 i32)))
+  (type (;3;) (func (param i64) (result f32)))
+  (type (;4;) (func (param f32 f32) (result i32)))
+  (type (;5;) (func (param f32 f32 f32 f32 f32 f32 f32 f32 f32 f32 f32 f32 i32)))
+  (table (;0;) 1 1 funcref)
+  (memory (;0;) 16)
+  (global $__stack_pointer (;0;) (mut i32) i32.const 1048576)
+  (export "memory" (memory 0))
+  (export "entrypoint" (func $entrypoint))
+  (func $<miden_stdlib_sys::intrinsics::word::Word as core::cmp::PartialEq>::eq (;0;) (type 0) (param i32 i32) (result i32)
+    (local i32)
+    i32.const 0
+    local.set 2
+    block ;; label = @1
+      local.get 0
+      f32.load
+      local.get 1
+      f32.load
+      call $intrinsics::felt::eq
+      i32.const 1
+      i32.ne
+      br_if 0 (;@1;)
+      local.get 0
+      f32.load offset=4
+      local.get 1
+      f32.load offset=4
+      call $intrinsics::felt::eq
+      i32.const 1
+      i32.ne
+      br_if 0 (;@1;)
+      local.get 0
+      f32.load offset=8
+      local.get 1
+      f32.load offset=8
+      call $intrinsics::felt::eq
+      i32.const 1
+      i32.ne
+      br_if 0 (;@1;)
+      local.get 0
+      f32.load offset=12
+      local.get 1
+      f32.load offset=12
+      call $intrinsics::felt::eq
+      i32.const 1
+      i32.eq
+      local.set 2
+    end
+    local.get 2
+  )
+  (func $entrypoint (;1;) (type 1) (param f32 f32 f32 f32 f32 f32 f32 f32 f32 f32 f32 f32)
+    (local i32 i32 i32)
+    global.get $__stack_pointer
+    local.tee 12
+    local.set 13
+    local.get 12
+    i32.const 96
+    i32.sub
+    i32.const -32
+    i32.and
+    local.tee 12
+    global.set $__stack_pointer
+    local.get 3
+    local.get 2
+    local.get 1
+    local.get 0
+    local.get 7
+    local.get 6
+    local.get 5
+    local.get 4
+    local.get 11
+    local.get 10
+    local.get 9
+    local.get 8
+    local.get 12
+    i32.const 32
+    i32.add
+    call $std::collections::smt::set
+    local.get 12
+    local.get 12
+    i64.load offset=40
+    i64.store offset=72
+    local.get 12
+    local.get 12
+    i64.load offset=32
+    i64.store offset=64
+    local.get 12
+    local.get 12
+    i64.load offset=56
+    i64.store offset=88
+    local.get 12
+    local.get 12
+    i64.load offset=48
+    i64.store offset=80
+    local.get 12
+    local.get 12
+    i32.const 64
+    i32.add
+    call $miden_stdlib_sys::intrinsics::word::Word::reverse
+    local.get 12
+    i32.const 16
+    i32.add
+    local.tee 14
+    local.get 12
+    i32.const 80
+    i32.add
+    call $miden_stdlib_sys::intrinsics::word::Word::reverse
+    i64.const 0
+    call $intrinsics::felt::from_u64_unchecked
+    local.set 8
+    i64.const 0
+    call $intrinsics::felt::from_u64_unchecked
+    local.set 9
+    i64.const 0
+    call $intrinsics::felt::from_u64_unchecked
+    local.set 10
+    local.get 12
+    i64.const 0
+    call $intrinsics::felt::from_u64_unchecked
+    f32.store offset=92
+    local.get 12
+    local.get 10
+    f32.store offset=88
+    local.get 12
+    local.get 9
+    f32.store offset=84
+    local.get 12
+    local.get 8
+    f32.store offset=80
+    i64.const -8162549007765693629
+    call $intrinsics::felt::from_u64_unchecked
+    local.set 8
+    i64.const 2968936853898945160
+    call $intrinsics::felt::from_u64_unchecked
+    local.set 9
+    i64.const 4556576161526575002
+    call $intrinsics::felt::from_u64_unchecked
+    local.set 10
+    local.get 12
+    i64.const 900237903234600995
+    call $intrinsics::felt::from_u64_unchecked
+    f32.store offset=44
+    local.get 12
+    local.get 10
+    f32.store offset=40
+    local.get 12
+    local.get 9
+    f32.store offset=36
+    local.get 12
+    local.get 8
+    f32.store offset=32
+    block ;; label = @1
+      local.get 12
+      local.get 12
+      i32.const 80
+      i32.add
+      call $<miden_stdlib_sys::intrinsics::word::Word as core::cmp::PartialEq>::eq
+      i32.eqz
+      br_if 0 (;@1;)
+      local.get 14
+      local.get 12
+      i32.const 32
+      i32.add
+      call $<miden_stdlib_sys::intrinsics::word::Word as core::cmp::PartialEq>::eq
+      i32.eqz
+      br_if 0 (;@1;)
+      local.get 13
+      global.set $__stack_pointer
+      return
+    end
+    unreachable
+  )
+  (func $miden_stdlib_sys::intrinsics::word::Word::reverse (;2;) (type 2) (param i32 i32)
+    (local i32 i32 i32 f32)
+    global.get $__stack_pointer
+    i32.const 16
+    i32.sub
+    local.tee 2
+    local.get 1
+    i64.load offset=8
+    i64.store offset=8 align=4
+    local.get 2
+    local.get 1
+    i64.load
+    i64.store align=4
+    local.get 2
+    i32.const 12
+    i32.add
+    local.set 3
+    i32.const 0
+    local.set 1
+    block ;; label = @1
+      loop ;; label = @2
+        local.get 1
+        i32.const 8
+        i32.eq
+        br_if 1 (;@1;)
+        local.get 2
+        local.get 1
+        i32.add
+        local.tee 4
+        f32.load
+        local.set 5
+        local.get 4
+        local.get 3
+        i32.load
+        i32.store
+        local.get 3
+        local.get 5
+        f32.store
+        local.get 1
+        i32.const 4
+        i32.add
+        local.set 1
+        local.get 3
+        i32.const -4
+        i32.add
+        local.set 3
+        br 0 (;@2;)
+      end
+    end
+    local.get 0
+    local.get 2
+    i64.load offset=8 align=4
+    i64.store offset=8
+    local.get 0
+    local.get 2
+    i64.load align=4
+    i64.store
+  )
+  (func $intrinsics::felt::from_u64_unchecked (;3;) (type 3) (param i64) (result f32)
+    unreachable
+  )
+  (func $intrinsics::felt::eq (;4;) (type 4) (param f32 f32) (result i32)
+    unreachable
+  )
+  (func $std::collections::smt::set (;5;) (type 5) (param f32 f32 f32 f32 f32 f32 f32 f32 f32 f32 f32 f32 i32)
+    unreachable
+  )
+)
---
+++ tests/integration/src/rust_masm_tests/rust_sdk/stdlib/collections.rs
@@ -0,0 +1,210 @@
+use miden_core::{
+    crypto::merkle::{MerkleStore, Smt},
+    Felt, FieldElement, Word,
+};
+use miden_processor::AdviceInputs;
+use midenc_debug::Executor;
+use midenc_expect_test::expect_file;
+use midenc_frontend_wasm::WasmTranslationConfig;
+
+use crate::CompilerTest;
+
+fn felt(value: u64) -> Felt {
+    Felt::new(value)
+}
+
+fn word(e0: u64, e1: u64, e2: u64, e3: u64) -> Word {
+    [felt(e0), felt(e1), felt(e2), felt(e3)].into()
+}
+
+fn build_advice_inputs_for_smt(smt: &Smt) -> AdviceInputs {
+    let store = MerkleStore::from(smt);
+    let map = smt
+        .leaves()
+        .map(|(_, leaf)| (leaf.hash(), leaf.to_elements()))
+        .collect::<Vec<_>>();
+
+    AdviceInputs::default()
+        .with_stack([Felt::ZERO])
+        .with_map(map)
+        .with_merkle_store(store)
+}
+
+fn word_components(word: Word) -> [Felt; 4] {
+    word.into()
+}
+
+fn word_to_u64s(word: Word) -> [u64; 4] {
+    let [a, b, c, d] = word_components(word);
+    [a.as_int(), b.as_int(), c.as_int(), d.as_int()]
+}
+
+fn push_word_args(args: &mut Vec<Felt>, word: Word) {
+    let [a, b, c, d] = word_components(word);
+    args.push(d);
+    args.push(c);
+    args.push(b);
+    args.push(a);
+}
+
+#[test]
+fn test_smt_get_binding() {
+    let entries = [
+        (word(1, 2, 3, 100), word(10, 11, 12, 13)),
+        (word(5, 6, 7, 200), word(20, 21, 22, 23)),
+        (word(8, 9, 10, 200), word(30, 31, 32, 33)),
+    ];
+    let smt = Smt::with_entries(entries).expect("smt initialization failed");
+    let (key, expected_value) = entries[0];
+    let root = smt.root();
+
+    let expected_value_u64 = word_to_u64s(expected_value);
+
+    let main_fn = format!(
+        r#"
+    (
+        k0: Felt,
+        k1: Felt,
+        k2: Felt,
+        k3: Felt,
+        r0: Felt,
+        r1: Felt,
+        r2: Felt,
+        r3: Felt
+    ) {{
+        let key = Word::new([k0, k1, k2, k3]);
+        let root = Word::new([r0, r1, r2, r3]);
+        let result = smt_get(key, root);
+        let value = result.value;
+        let returned_root = result.root;
+
+        let expected = Word::new([
+            Felt::from_u64_unchecked({ev0}),
+            Felt::from_u64_unchecked({ev1}),
+            Felt::from_u64_unchecked({ev2}),
+            Felt::from_u64_unchecked({ev3}),
+        ]);
+        assert_eq!(value, expected, "smt_get returned unexpected value");
+        assert_eq!(returned_root, root, "smt_get should not mutate the root");
+    }}"#,
+        ev0 = expected_value_u64[0],
+        ev1 = expected_value_u64[1],
+        ev2 = expected_value_u64[2],
+        ev3 = expected_value_u64[3],
+    );
+
+    let config = WasmTranslationConfig::default();
+    let mut test = CompilerTest::rust_fn_body_with_stdlib_sys(
+        "rust_sdk_stdlib_smt_get",
+        &main_fn,
+        config,
+        ["--test-harness".into()],
+    );
+    test.expect_wasm(expect_file!["../../../../expected/rust_sdk_stdlib_smt_get.wat"]);
+    test.expect_ir(expect_file!["../../../../expected/rust_sdk_stdlib_smt_get.hir"]);
+    test.expect_masm(expect_file!["../../../../expected/rust_sdk_stdlib_smt_get.masm"]);
+
+    let package = test.compiled_package();
+
+    let advice_inputs = build_advice_inputs_for_smt(&smt);
+
+    let mut args = Vec::new();
+    push_word_args(&mut args, root);
+    push_word_args(&mut args, key);
+
+    let mut exec = Executor::for_package(&package, args.clone(), &test.session)
+        .expect("executor creation failed");
+    exec.with_advice_inputs(advice_inputs);
+
+    exec.execute(&package.unwrap_program(), &test.session);
+}
+
+#[test]
+fn test_smt_set_binding() {
+    let key = word(5, 6, 7, 200);
+    let new_value = word(40, 41, 42, 43);
+
+    let smt = Smt::new();
+    let root = smt.root();
+
+    let mut expected_smt = smt.clone();
+    let expected_old_value = expected_smt.insert(key, new_value);
+    let expected_new_root = expected_smt.root();
+
+    let expected_old_u64 = word_to_u64s(expected_old_value);
+    let expected_new_u64 = word_to_u64s(expected_new_root);
+
+    let main_fn = format!(
+        r#"
+    (
+        v0: Felt,
+        v1: Felt,
+        v2: Felt,
+        v3: Felt,
+        k0: Felt,
+        k1: Felt,
+        k2: Felt,
+        k3: Felt,
+        r0: Felt,
+        r1: Felt,
+        r2: Felt,
+        r3: Felt
+    ) {{
+        let value = Word::new([v0, v1, v2, v3]);
+        let key = Word::new([k0, k1, k2, k3]);
+        let root = Word::new([r0, r1, r2, r3]);
+        let result = smt_set(value, key, root);
+        let old_value = result.old_value;
+        let new_root = result.new_root;
+
+        let expected_old = Word::new([
+            Felt::from_u64_unchecked({eo0}),
+            Felt::from_u64_unchecked({eo1}),
+            Felt::from_u64_unchecked({eo2}),
+            Felt::from_u64_unchecked({eo3}),
+        ]);
+        let expected_new = Word::new([
+            Felt::from_u64_unchecked({en0}),
+            Felt::from_u64_unchecked({en1}),
+            Felt::from_u64_unchecked({en2}),
+            Felt::from_u64_unchecked({en3}),
+        ]);
+        assert_eq!(old_value, expected_old, "smt_set returned unexpected old value");
+        assert_eq!(new_root, expected_new, "smt_set returned unexpected new root");
+    }}"#,
+        eo0 = expected_old_u64[0],
+        eo1 = expected_old_u64[1],
+        eo2 = expected_old_u64[2],
+        eo3 = expected_old_u64[3],
+        en0 = expected_new_u64[0],
+        en1 = expected_new_u64[1],
+        en2 = expected_new_u64[2],
+        en3 = expected_new_u64[3],
+    );
+
+    let config = WasmTranslationConfig::default();
+    let mut test = CompilerTest::rust_fn_body_with_stdlib_sys(
+        "rust_sdk_stdlib_smt_set",
+        &main_fn,
+        config,
+        ["--test-harness".into()],
+    );
+    test.expect_wasm(expect_file!["../../../../expected/rust_sdk_stdlib_smt_set.wat"]);
+    test.expect_ir(expect_file!["../../../../expected/rust_sdk_stdlib_smt_set.hir"]);
+    test.expect_masm(expect_file!["../../../../expected/rust_sdk_stdlib_smt_set.masm"]);
+
+    let package = test.compiled_package();
+
+    let advice_inputs = build_advice_inputs_for_smt(&smt);
+
+    let mut args = Vec::new();
+    push_word_args(&mut args, root);
+    push_word_args(&mut args, key);
+    push_word_args(&mut args, new_value);
+
+    let mut exec = Executor::for_package(&package, args.clone(), &test.session)
+        .expect("executor creation failed");
+    exec.with_advice_inputs(advice_inputs);
+
+    exec.execute(&package.unwrap_program(), &test.session);
+}
