24b3c936b3debb5464b5fbc5daf368362dd85435
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2026-02-08 12:43:49 -0500

    wip: change pass order to improve quality of generated code

--- midenc-compile/src/stages/rewrite.rs
+++ midenc-compile/src/stages/rewrite.rs
@@ -7,7 +7,10 @@ use midenc_hir::{
     pass::{IRPrintingConfig, Nesting, PassManager},
     patterns::{GreedyRewriteConfig, RegionSimplificationLevel},
 };
-use midenc_hir_transform::{Canonicalizer, ControlFlowSink, SinkOperandDefs};
+use midenc_hir_transform::{
+    Canonicalizer, CommonSubexpressionElimination, SinkOperandDefs,
+    SparseConditionalConstantPropagation,
+};
 
 use super::*;
 
@@ -63,27 +66,37 @@ impl Stage for ApplyRewritesStage {
                 let mut module_pm = component_pm.nest::<builtin::Module>();
                 let mut func_pm = module_pm.nest::<builtin::Function>();
                 func_pm.add_pass(Canonicalizer::create_with_config(&rewrite_config));
+                func_pm.add_pass(Box::new(CommonSubexpressionElimination));
+                //func_pm.add_pass(Box::new(SinkOperandDefs));
+                func_pm.add_pass(Box::new(SparseConditionalConstantPropagation));
                 func_pm.add_pass(Box::new(SinkOperandDefs));
-                func_pm.add_pass(Box::new(ControlFlowSink));
+                //func_pm.add_pass(Box::new(ControlFlowSink));
                 func_pm.add_pass(Box::new(TransformSpills));
                 func_pm.add_pass(Box::new(LiftControlFlowToSCF));
                 // Re-run canonicalization to clean up generated structured control flow
                 func_pm.add_pass(Canonicalizer::create_with_config(&rewrite_config));
                 func_pm.add_pass(Box::new(SinkOperandDefs));
-                func_pm.add_pass(Box::new(ControlFlowSink));
+                func_pm.add_pass(Box::new(TransformSpills));
+                //func_pm.add_pass(Box::new(ControlFlowSink));
+                //func_pm.add_pass(Box::new(DeadCodeElimination));
             }
             // Function passes for component-level functions
             {
                 let mut func_pm = component_pm.nest::<builtin::Function>();
                 func_pm.add_pass(Canonicalizer::create_with_config(&rewrite_config));
+                func_pm.add_pass(Box::new(CommonSubexpressionElimination));
+                //func_pm.add_pass(Box::new(SinkOperandDefs));
+                //func_pm.add_pass(Box::new(ControlFlowSink));
+                func_pm.add_pass(Box::new(SparseConditionalConstantPropagation));
                 func_pm.add_pass(Box::new(SinkOperandDefs));
-                func_pm.add_pass(Box::new(ControlFlowSink));
                 func_pm.add_pass(Box::new(TransformSpills));
                 func_pm.add_pass(Box::new(LiftControlFlowToSCF));
                 // Re-run canonicalization to clean up generated structured control flow
                 func_pm.add_pass(Canonicalizer::create_with_config(&rewrite_config));
                 func_pm.add_pass(Box::new(SinkOperandDefs));
-                func_pm.add_pass(Box::new(ControlFlowSink));
+                func_pm.add_pass(Box::new(TransformSpills));
+                //func_pm.add_pass(Box::new(ControlFlowSink));
+                //func_pm.add_pass(Box::new(DeadCodeElimination));
             }
         }
 
