51d628df0d10b1cc7d8cb38f050d8c4aefbd675d
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2026-04-15 10:03:17 +0300

    test: update counter contract note cycle expectations

--- tests/integration-network/src/mockchain/counter_contract_no_auth.rs
+++ tests/integration-network/src/mockchain/counter_contract_no_auth.rs
@@ -105,7 +105,7 @@ pub fn test_counter_contract_no_auth() {
         .unwrap();
     let tx_measurements = execute_tx(&mut chain, tx_context_builder);
     expect!["1824"].assert_eq(auth_procedure_cycles(&tx_measurements));
-    expect!["28392"].assert_eq(note_cycles(&tx_measurements, counter_note.id()));
+    expect!["28585"].assert_eq(note_cycles(&tx_measurements, counter_note.id()));
 
     // The counter contract storage value should be 2 after the note is consumed
     assert_counter_storage(
