ff6e2256bfef27905b0fc9650150fa71ca97388e
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2026-04-27 13:38:49 +0300

    chore: switch to `profile.release.opt-level = "s"` #1065

--- tests/integration/src/rust_masm_tests/examples.rs
+++ tests/integration/src/rust_masm_tests/examples.rs
@@ -329,7 +329,7 @@ fn basic_wallet_and_p2id() {
         CompilerTest::rust_source_cargo_miden("../../examples/basic-wallet", config.clone(), []);
     let account_package = account_test.compile_package();
     assert!(account_package.is_library(), "expected library");
-    expect!["29921"].assert_eq(stripped_mast_size_str(&account_package));
+    expect!["29016"].assert_eq(stripped_mast_size_str(&account_package));
 
     let mut tx_script_test = CompilerTest::rust_source_cargo_miden(
         "../../examples/basic-wallet-tx-script",
@@ -338,19 +338,19 @@ fn basic_wallet_and_p2id() {
     );
     let tx_script_package = tx_script_test.compile_package();
     assert!(tx_script_package.is_program(), "expected program");
-    expect!["50961"].assert_eq(stripped_mast_size_str(&tx_script_package));
+    expect!["45884"].assert_eq(stripped_mast_size_str(&tx_script_package));
 
     let mut p2id_test =
         CompilerTest::rust_source_cargo_miden("../../examples/p2id-note", config.clone(), []);
     let note_package = p2id_test.compile_package();
     assert!(note_package.is_library(), "expected library");
-    expect!["44348"].assert_eq(stripped_mast_size_str(&note_package));
+    expect!["38430"].assert_eq(stripped_mast_size_str(&note_package));
 
     let mut p2ide_test =
         CompilerTest::rust_source_cargo_miden("../../examples/p2ide-note", config, []);
     let p2ide_package = p2ide_test.compile_package();
     assert!(p2ide_package.is_library(), "expected library");
-    expect!["51565"].assert_eq(stripped_mast_size_str(&p2ide_package));
+    expect!["43003"].assert_eq(stripped_mast_size_str(&p2ide_package));
 }
 
 #[test]
