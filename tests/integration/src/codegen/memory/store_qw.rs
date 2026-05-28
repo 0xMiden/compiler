0f5350c332e16041da4972f615a8c85b500b3e1c
Moritz Zielke <moritz.zielke@gmail.com>
2026-05-28 11:58:49 +0200

    chore: cover all offsets in `store_qw_unaligned_impl`

--- tests/integration/src/codegen/memory/store_qw.rs
+++ tests/integration/src/codegen/memory/store_qw.rs
@@ -30,12 +30,13 @@ fn store_qw_unaligned_impl<T: QuadwordIO>(write_val: T) {
     });
 
     let run_test = |offs: u32| {
-        // Write known 20 bytes (5xu32) at `addr` before running the module.
+        // Write known 32 bytes (8xu32) at `addr` before running the module.
         // Run the module with different offsets and afterwards verify storing 16 bytes inside the
         // module has modified memory as expected.
         let initial_bytes = [
             0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
-            0x0f, 0x10, 0x11, 0x12, 0x13, 0x14,
+            0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c,
+            0x1d, 0x1e, 0x1f, 0x20,
         ];
         let initializers = [Initializer::MemoryBytes {
             addr: write_to,
@@ -51,6 +52,9 @@ fn store_qw_unaligned_impl<T: QuadwordIO>(write_val: T) {
             u32::from_le_bytes(expected_bytes[8..12].try_into().unwrap()),
             u32::from_le_bytes(expected_bytes[12..16].try_into().unwrap()),
             u32::from_le_bytes(expected_bytes[16..20].try_into().unwrap()),
+            u32::from_le_bytes(expected_bytes[20..24].try_into().unwrap()),
+            u32::from_le_bytes(expected_bytes[24..28].try_into().unwrap()),
+            u32::from_le_bytes(expected_bytes[28..32].try_into().unwrap()),
         ];
 
         let output = eval_package::<u32, _, _>(
@@ -65,6 +69,9 @@ fn store_qw_unaligned_impl<T: QuadwordIO>(write_val: T) {
                     trace.read_from_rust_memory::<u32>(write_to + 8).unwrap(),
                     trace.read_from_rust_memory::<u32>(write_to + 12).unwrap(),
                     trace.read_from_rust_memory::<u32>(write_to + 16).unwrap(),
+                    trace.read_from_rust_memory::<u32>(write_to + 20).unwrap(),
+                    trace.read_from_rust_memory::<u32>(write_to + 24).unwrap(),
+                    trace.read_from_rust_memory::<u32>(write_to + 28).unwrap(),
                 ];
 
                 for (index, (actual, expected)) in actual.iter().zip(expected.iter()).enumerate() {
@@ -83,7 +90,7 @@ fn store_qw_unaligned_impl<T: QuadwordIO>(write_val: T) {
         assert_eq!(output, 1);
     };
 
-    for offs in 0..=3 {
+    for offs in 0..=15 {
         run_test(offs);
     }
 }
